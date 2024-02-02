7b4152b700f082bf7f99f7c86fd5d16ad78bf606
Caravalatcoding <136609039+Caravalatcoding@users.noreply.github.com>
2024-02-02 18:56:54 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+function preload()
+{
+}
+
+function setup()
+{
+    canvas = createCanvas(500, 500)
+    canvas.center()
+}
+function draw()
+{
+
+}
+
+function onClick()
+{
+    save('Filtered-image-by-Me.jpg')
+}
