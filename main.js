bb835f322cc8c54d58cd62edea5feac70d6b0360
Caravalatcoding <136609039+Caravalatcoding@users.noreply.github.com>
2024-03-13 11:24:07 +0530

    Update main.js

--- main.js
+++ main.js
@@ -4,15 +4,37 @@ function preload()
 
 function setup()
 {
-    canvas = createCanvas(500, 500)
-    canvas.center()
+    canvas = createCanvas(500, 500);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(500, 500);
+    video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
 }
 function draw()
 {
+    image(video, 0, 0, 500, 500);
 
 }
 
 function onClick()
 {
     save('Filtered-image-by-Me.jpg')
+}
+
+function modelLoaded()
+{
+    console.log('Posenet is Initialized');
+}
+
+function gotPoses()
+{
+    if(results.length > 0)
+    {
+        console.log(results);
+        console.log("nose x = " + results[0].pose.nose.x);
+        console.log("nose y = " + results[0].pose.nose.y);
+    }
 }
